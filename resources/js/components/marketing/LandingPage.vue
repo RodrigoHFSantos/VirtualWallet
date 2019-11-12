ba4a531b57a6d7c29534481c42472795e50faa31
Apache96 <39128225+Apache96@users.noreply.github.com>
2019-11-12 23:03:56 +0000

    show total walltes in LandingPage

--- resources/js/components/marketing/LandingPage.vue
+++ resources/js/components/marketing/LandingPage.vue
@@ -1,5 +1,34 @@
 <template>
-    <div class="flex-center">
-        Landing Page
+	<div>
+	    <div id=""class="flex-center"> 
+	        Landing Page
+	    </div>
+	    <div id=""class="flex-center">
+	    	Total Wallets -> {{totalWallets}}
+	    </div>
     </div>
 </template>
+
+<script>
+	export default{
+		data: function(){
+			return{
+				totalWallets: null
+			}
+		},
+
+		methods:{
+			getTotalWallets:function () {
+				axios.get('api/wallets/total')
+					.then(response=>{
+						console.log(response);
+						this.totalWallets = response.data;
+					})
+			}
+		},
+		
+		mounted(){
+			this.getTotalWallets();
+		}
+	}
+</script>
