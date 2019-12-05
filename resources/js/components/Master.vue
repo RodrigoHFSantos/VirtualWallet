6c6dcd9010226f55e7cf8be69fe8b7fe4f761361
Rodrigo Santos <rodrigohfsantos@hotmail.com>
2019-12-05 22:05:51 +0000

    Getters for roles done
    Register an income (credit) movement from the
    outside of the platform only for operator role. (US.6)

--- resources/js/components/Master.vue
+++ resources/js/components/Master.vue
@@ -17,7 +17,7 @@
           <v-icon>mdi-information</v-icon>
         </v-btn>
 
-        <v-btn icon color="white" :to="{ name: 'operator-movement-income' }">
+        <v-btn v-if="loggedIn && isOperator" icon color="white" :to="{ name: 'operator-movement-income' }">
           <v-icon>mdi-cached</v-icon>
         </v-btn>
 
@@ -52,6 +52,9 @@ export default {
    computed: {
      loggedIn: function(){
        return this.$store.getters.loggedIn;
+     },
+     isOperator: function(){
+       return this.$store.getters.isOperator;
      }
    },
    methods: {
